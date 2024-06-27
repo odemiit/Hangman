42f2d22a909c216d99a46d4375cf7d220b2026b4
Odemi Tosanwumi <46735063+odemiit@users.noreply.github.com>
2024-06-27 18:45:29 +0000

    importing the stages and word list from hangman_art and hangman_words

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 #Step 5
 
 import random
+import stages
+import word_list
 
 #TODO-1: - Update the word list to use the 'word_list' from hangman_words.py
 #Delete this line: word_list = ["ardvark", "baboon", "camel"]
