b024f3fd99868da2ba8a12c98590ecb462d3b150
Odemi Tosanwumi <46735063+odemiit@users.noreply.github.com>
2024-06-27 14:02:51 +0000

    Task 3 start for loop adjusted to display current position, letter and users guess.

--- main.py
+++ main.py
@@ -8,19 +8,18 @@ chosen_word = random.choice(word_list)
 print(f'Pssst, the solution is {chosen_word}.')
 
 #For each letter in the chosen_word, add a "_" to 'display'.
-guess = input("Guess a letter: ").lower()
 display = []
-
 for word in chosen_word:
   display += "_"
 
 #TODO-1: - Use a while loop to let the user guess again. The loop should only stop once the user has guessed all the letters in the chosen_word and 'display' has no more blanks ("_"). Then you can tell the user they've won.
-
+guess = input("Guess a letter: ").lower()
 
 #Loop through each position in the chosen_word; If the letter at that position matches 'guess' then reveal that letter in the display at that position.
 start = 0
 
 for letter in chosen_word:
+  print(f"Current position: {start}\n Current letter: {letter}\n Guessed letter: {guess}")
   if guess == letter:
     display[start] = guess
   start += 1
